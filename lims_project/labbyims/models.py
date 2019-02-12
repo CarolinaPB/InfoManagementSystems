d366ab16fbc2b121272658495049cd3c7fbe49e7
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2019-02-12 16:52:18 +0100

    Admin can now add info to tables

--- lims_project/labbyims/models.py
+++ lims_project/labbyims/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=255, blank=True)
