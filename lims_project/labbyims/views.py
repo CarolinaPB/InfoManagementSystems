2d0a3c30087a63c65defe970523345cc9def528d
CarolinaPB <carolina.pitabarros@gmail.com>
2019-02-12 17:34:35 +0100

    added front page before login, front page after login

--- lims_project/labbyims/views.py
+++ lims_project/labbyims/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.views import View
 
 def home(request):
-    return render(request, 'labbyims/base.html')
+    return render(request, 'labbyims/home_afterlogin.html')
 
 def no_login(request):
     return render(request, 'labbyims/no_login.html')
