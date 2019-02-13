9676fdde2c02a496faf7124ede9f409e17b225bc
kathiwaury <47524923+kathiwaury@users.noreply.github.com>
2019-02-13 20:20:33 +0100

    add_item and add_product added

--- lims_project/labbyims/urls.py
+++ lims_project/labbyims/urls.py
@@ -9,7 +9,8 @@ from labbyims.views import search_results
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('', views.no_login, name='no_login'),
-
+	path('add_product/', views.add_product, name='add_product'),
+	path('add_item/', views.add_item, name='add_item'),
     path('search_results/', search_results.as_view(), name='search_results')
 
 ]
--- lims_project/labbyims/views.py
+++ lims_project/labbyims/views.py
@@ -11,7 +11,12 @@ def home(request):
 def no_login(request):
     return render(request, 'labbyims/no_login.html')
 
-
+def add_product(request):
+	return render(request, 'labbyims/add_product.html')
+	
+def add_item(request):
+	return render(request, 'labbyims/add_item.html')
+	
 class search_results(View):
 
     def get(self, request, *args, **kwargs):
