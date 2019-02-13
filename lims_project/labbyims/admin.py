3583b4dd455416e5f4a21fceba94303c7de45f2b
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-02-13 09:53:28 +0100

    added missing things

--- lims_project/labbyims/admin.py
+++ lims_project/labbyims/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Account, Product, Room, Location, Product_Unit, Reserve
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Product)
+admin.site.register(Room)
+admin.site.register(Location)
+admin.site.register(Product_Unit)
+admin.site.register(Reserve)
