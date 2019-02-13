ed98aaa011359f29b5e0cb44a24089dcaf9ae660
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-02-13 09:38:48 +0100

    added views to homepage before and after login

--- lims_project/labbyims/urls.py
+++ lims_project/labbyims/urls.py
@@ -7,7 +7,8 @@ from . import views
 from labbyims.views import search_results
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('', views.no_login, name='no_login'),
 
     path('search_results/', search_results.as_view(), name='search_results')
 
