7a59ef94fe6c7128dd25a693949c4d94d2768d2f
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-02-14 10:35:25 +0100

    add missing lines

--- lims_project/labbyims/urls.py
+++ lims_project/labbyims/urls.py
@@ -1,16 +1,15 @@
-# Create app specific urls here.
-
 from django.urls import path, include
-
 from . import views
-
 from labbyims.views import search_results
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('', views.no_login, name='no_login'),
-	path('add_product/', views.add_product, name='add_product'),
-	path('add_item/', views.add_item, name='add_item'),
+    path('login/', auth_views.LoginView.as_view(template_name='labbyims/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('add_product/', views.add_product, name='add_product'),	
+    path('add_item/', views.add_item, name='add_item'),
     path('search_results/', search_results.as_view(), name='search_results')
 
 ]
