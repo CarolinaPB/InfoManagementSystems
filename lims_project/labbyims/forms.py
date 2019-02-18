8efd1ae80b3ae13df5c623002f6f48f77b9adb3c
CarolinaPB <carolina.pitabarros@gmail.com>
2019-02-18 11:36:11 +0100

    small changes

--- lims_project/labbyims/forms.py
+++ lims_project/labbyims/forms.py
@@ -3,14 +3,15 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
 class AdvancedSearch(forms.Form):
-    search = forms.CharField(widget=forms.TextInput(attrs={'class': 'col-md-12'}))
-    CHOICES=[('unit', 'Unit'),
+    search = forms.CharField(widget=forms.TextInput(attrs={'class': 'col-md-12'}), label=False)
+    CHOICES=[('advanced_search', "Choose one"),
+             ('unit', 'Unit'),
              ('location','Location'),
              ('item_type','Item type'),
              ('finished', 'Finished')
              ]
-    advanced_search = forms.ChoiceField(choices=CHOICES)
-    advanced_search.widget.attrs.update({'class': 'col-md-4'})
+    advanced_search = forms.ChoiceField(choices=CHOICES, label=False)
+    advanced_search.widget.attrs.update({'class': 'col-md-6'})
     advanced_search.widget.attrs.update({'name': 'advanced_search'})
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
