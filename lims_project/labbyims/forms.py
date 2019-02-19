e9ebb7bfe3af90274c15897770469b9b8ab3c061
CarolinaPB <carolina.pitabarros@gmail.com>
2019-02-19 12:32:37 +0100

    integrated with kathi's

--- lims_project/labbyims/forms.py
+++ lims_project/labbyims/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
+from django.db import models
+from .models import Product_Unit, Product, Location
+from django.forms.widgets import DateInput
 
 class AdvancedSearch(forms.Form):
     search = forms.CharField(widget=forms.TextInput(attrs={'class': 'col-md-12'}), label=False)
-    CHOICES=[('advanced_search', "Choose one"),
+    CHOICES=[
              ('unit', 'Unit'),
              ('location','Location'),
              ('item_type','Item type'),
@@ -39,3 +42,14 @@ class BasicSearch(forms.Form):
             "search",
             Submit("submit", "Search", css_class="btn")
         )
+
+class Product_UnitForm(forms.ModelForm):
+    class Meta:
+        model = Product_Unit
+        exclude = ['reservation']
+        widgets = {
+            "del_date":DateInput(attrs = {"type":"date"}),
+            "open_date":DateInput(attrs = {"type":"date"}),
+            "exp_date":DateInput(attrs = {"type":"date"}),
+            "ret_date":DateInput(attrs = {"type":"date"}),
+        }
--- lims_project/labbyims/views.py
+++ lims_project/labbyims/views.py
@@ -1,11 +1,10 @@
-# Create your views here.
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
 from django.views import View
-from .forms import AdvancedSearch, BasicSearch
+from .forms import AdvancedSearch, BasicSearch, Product_UnitForm
+from .models import Product_Unit
 
 def home(request):
     if request.method == 'POST':
@@ -34,7 +33,18 @@ def add_product(request):
 	return render(request, 'labbyims/add_product.html')
 
 def add_item(request):
-	return render(request, 'labbyims/add_item.html')
+    if request.method == "POST":
+        form = Product_UnitForm(request.POST or None)
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, 'labbyims/home_afterlogin.html')
+        else:
+            print(form.errors)
+    else:
+        form = Product_UnitForm()
+
+    context = {'form': form}
+    return render(request, 'labbyims/add_item.html', context)
 
 class search_results(View):
 
