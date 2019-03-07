9a2c53904af1440a49a8c7ba1a1185d179e2ef31
kathiwaury <47524923+kathiwaury@users.noreply.github.com>
2019-03-07 10:14:38 +0100

    views with updated add_item + add_item_cas

--- lims_project/labbyims/views.py
+++ lims_project/labbyims/views.py
@@ -2,44 +2,59 @@
 
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from labbyims.forms import SignUpForm
-from django.http import HttpResponseRedirect
-from django.db.models import F
+
+from django.db.models import F,Q, FloatField
+from django.db.models.functions import Cast
+
 from django.views import View
-from .forms import AdvancedSearch, Product_UnitForm, Product_Form, Location_Form
-from .models import Product_Unit, Product, Location, Room
-from .tables import Product_UnitTable, LocationTable, Product_Unit_ExpTable, FP_Product_UnitTable
-from .forms import AdvancedSearch, Product_UnitForm, Product_Form, Location_Form, Room_Form, Reserve_Form
-from .models import Product_Unit, Product, Location, Room, Reserve, User
-from .tables import Product_UnitTable, LocationTable, Product_Unit_ExpTable
+from .forms import AdvancedSearch, Product_UnitForm, Product_Form, Location_Form, Room_Form, Reserve_Form, Department_Form
+from .tables import Product_UnitTable, LocationTable, Product_Unit_ExpTable, FP_Product_UnitTable, Product_Unit_MyTable, FP_ReserveTable, ReserveTable, FP_Running_LowTable, Running_LowTable
+from .models import Product_Unit, Product, Location, Room, Reserve, User, Watching, Department
 from django_tables2 import RequestConfig
-from .filters import ProductFilter, LocationFilter, ProductCASFilter
 import datetime
 from datetime import datetime, timedelta
 from django.utils import timezone
+from .filters import ProductFilter, LocationFilter, Prod_ResFilter, ProductCASFilter
+from decimal import Decimal
+
+n = []
 
 def home(request):
-    if request.method == 'POST':
-        form = AdvancedSearch(request.POST)
-        if form.is_valid():
-            search_res = form.cleaned_data["search"]
-            print(search_res)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AdvancedSearch(request.POST)
+            if form.is_valid():
+                search_res = form.cleaned_data["search"]
+            else:
+                print(form.errors)
+
+            return HttpResponseRedirect('/search/?description={}'.format(search_res))
+
         else:
-            print(form.errors)
+            form = AdvancedSearch(initial=request.GET)
 
-        return HttpResponseRedirect('/search/?description={}'.format(search_res))
+        current_date = timezone.now()
+        warning = current_date + timedelta(days=27)
 
+        exp_filter = Product_Unit.objects.filter(Q(is_inactive=False), \
+                    Q(exp_date__range = [current_date, warning ]) | \
+                    Q(ret_date__range =[current_date, warning ]) )
+        table_exp = FP_Product_UnitTable(exp_filter, prefix="1-")
+        RequestConfig(request,paginate={'per_page': 3} ).configure(table_exp)
+
+        res_list=Reserve.objects.filter(Q(user_id= request.user),\
+                    Q(date_res__range = [current_date, warning ])).select_related()
+        table_res = FP_ReserveTable(res_list, prefix="2-")
+        RequestConfig(request).configure(table_res)
+
+        return render(request, 'labbyims/home_afterlogin.html',{'form':form, \
+                    'table_res':table_res, 'table_exp': table_exp,},)
     else:
-        form = AdvancedSearch(initial=request.GET)
+        return render(request, 'labbyims/home_afterlogin.html')
 
-    current_date = timezone.now()
-    exp_warning = current_date + timedelta(days=27)
-    exp_filter = Product_Unit.objects.filter(is_inactive = False, exp_date__range = [current_date,exp_warning ])
-    table_exp = FP_Product_UnitTable(exp_filter)
-    RequestConfig(request).configure(table_exp)
-    return render(request, 'labbyims/home_afterlogin.html' ,{'form':form, 'table_exp': table_exp},)
-    
+
 
 def no_login(request):
     return render(request, 'labbyims/no_login.html')
@@ -60,32 +75,49 @@ def add_product(request):
 
 
 def add_item(request):
-    if request.method == "POST":
+    if request.method == "POST" and 'Submit' in request.POST:
+        print("getting to POST")
         form = Product_UnitForm(request.POST)
-        number = request.POST.get('number', False)
-        number = int(number)
         if form.is_valid():
+            number = int(request.POST.get('number', False))
+            low_warn_form = form.cleaned_data['low_warn_form']
+            dep_id_list = list(request.POST.getlist('department'))
+            print(dep_id_list)
             instance = form.save(commit=False)
             for i in range(0, number):
                 instance.pk = None
                 instance.save()
-            return HttpResponseRedirect('.')
-            #return render(request, 'labbyims/home.html')
+                j = 0
+                if dep_id_list != False:
+                    for j in range(0, len(dep_id_list)):
+                        dep_id = dep_id_list[j]
+                        #print(dep_id)
+                        dep = Department.objects.get(pk=dep_id)
+                        #print(dep)
+                        w = Watching(user = request.user, prod_un=instance, dept=dep, low_warn=low_warn_form)
+                        w.save()
+                        j += 1
+            return redirect("/home/")
         else:
             print(form.errors)
-    else:
-        form = Product_UnitForm()
 
-    return render(request, 'labbyims/add_item.html', {'form': form})
-
-def add_item_cas(request):
-    if request.method == "POST":
-        product_list = Product.objects.all()
-        product_filter = ProductCASFilter(request.GET, queryset=product_list)
-        print(product_filter)
-        return render(request, "labbyims/add_item.html", {'filter': product_filter})
     else:
-        return render(request, 'labbyims/add_item_cas.html')
+        cas_search = request.GET.get('Search', None)
+        print(cas_search)
+        prod_set = Product.objects.filter(cas=cas_search)
+        print(prod_set)
+        if not prod_set:
+            return render(request, 'labbyims/add_item_cas.html', {'text':"No product with this CAS number was found. Please try again."})
+        else:
+            for prod in prod_set:
+                prod_name = prod.name
+                prod_id = prod.id
+            form = Product_UnitForm(initial={'product': Product.objects.get(pk=prod_id)})
+
+    return render(request, 'labbyims/add_item.html', {'form': form, 'cas': cas_search, 'name':prod_name})
+
+def add_item_cas(request):   
+    return render(request, 'labbyims/add_item_cas.html')
 
 def inventory(request):
     table = Product_UnitTable(Product_Unit.objects.all())
@@ -114,13 +146,10 @@ def locations(request):
     RequestConfig(request).configure(table_1)
     return render(request, 'labbyims/locations.html', {'table_1': table_1})
 
-def expiring(request):
-    current_date = timezone.now()
-    exp_warning = current_date + timedelta(days=27)
-    exp_filter = Product_Unit.objects.filter(exp_date__range = [current_date,exp_warning ])
-    table_exp = Product_Unit_ExpTable(exp_filter)
-    RequestConfig(request).configure(table_exp)
-    return render(request, 'labbyims/expiring_retesting.html', {'table_exp': table_exp})
+def my_inventory(request):
+    table_my_inv = Product_Unit_MyTable(Product_Unit.objects.all())
+    RequestConfig(request).configure(table_my_inv)
+    return render(request, 'labbyims/my_inventory.html', {'table_my_inv': table_my_inv})
 
 def search(request):
     product_list = Product_Unit.objects.all()
@@ -131,7 +160,8 @@ def search(request):
 def search_location(request):
     location_list = Location.objects.all()
     location_filter = LocationFilter(request.GET, queryset=location_list)
-    return render(request, "labbyims/search_location.html", {'filter': location_filter})
+    return render(request, "labbyims/search_location.html",\
+                {'filter': location_filter})
 
 def add_room(request):
     if request.method == "POST":
@@ -147,14 +177,27 @@ def add_room(request):
     context = {'form': form}
     return render(request, 'labbyims/add_room.html', context)
 
-def add_reservation(request):
+def add_department(request):
     if request.method == "POST":
-        form = Reserve_Form(request.POST)
+        form = Department_Form(request.POST)
         if form.is_valid():
             form.save(commit=True)
             return HttpResponseRedirect('.')
         else:
             print(form.errors)
+    else:
+        form = Department_Form()
+    context = {'form': form}
+    return render(request, 'labbyims/add_department.html', context)
+    
+def add_reservation(request):
+    if request.method == "POST":
+        form = Reserve_Form(request.POST)
+        add_res = form.save(commit=False)
+        print(request.user)
+        add_res.user = request.user
+        add_res.save()
+        return HttpResponseRedirect('.')
     else:
         form = Reserve_Form()
 
@@ -162,7 +205,46 @@ def add_reservation(request):
     return render(request, 'labbyims/add_reservation.html', context)
 
 def reservations(request):
-    return render(request, 'labbyims/reservations.html')
+    current_date = timezone.now()
+    warning = current_date + timedelta(days=27)
+    res_list=Reserve.objects.filter(Q(user_id= request.user),\
+                    Q(date_res__range = [current_date, warning ])).select_related()
+    table_res = ReserveTable(res_list)
+    RequestConfig(request).configure(table_res)
+    return render(request, 'labbyims/reservations.html', {'table_res': table_res,}, )
+
+def running_low(request):
+    if request.user.is_authenticated:
+        run_low = F('init_amount')/2
+
+        ##prod = Product_Unit.object.get(curr_amount__lte =Cast(\
+                    #F('init_amount')/2, FloatField()))[0]
+
+
+
+
+        #prod = Product_Unit.objects.get()
+        watch_list = Watching.objects.filter(Q(),Q(user_id= request.user),\
+                    Q(low_warn = True),Q()).select_related()
+
+
+        table_watch = Running_LowTable(watch_list)
+        RequestConfig(request).configure(table_watch)
+        return render(request, 'labbyims/running_low.html', {'table_watch':table_watch,},)
+    else:
+        return render(request, 'labbyims/home_afterlogin.html')
 
 def about(request):
     return render(request, 'labbyims/about.html')
+
+def user_page(request):
+
+    model = User
+    template_name = 'labbyims/user_page.html'
+    def get_queryset():
+        return User.objects.all()
+    u = get_queryset()
+    print (u)
+    context={'user':u}
+
+    return render(request, 'labbyims/user_page.html', context)
