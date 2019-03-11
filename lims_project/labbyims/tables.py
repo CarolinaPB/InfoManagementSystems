fe46cb12335cf32bebd383f5ec2b8edc366d3a3d
CarolinaPB <carolina.pitabarros@gmail.com>
2019-03-11 10:45:47 +0100

    small changes

--- lims_project/labbyims/tables.py
+++ lims_project/labbyims/tables.py
@@ -58,9 +58,9 @@ class ReserveTable(tables.Table):
 class Product_Unit_MyTable(tables.Table):
     class Meta:
         model = Product_Unit
-        fields = ('description', 'product', 'purity', 'curr_amount', \
+        fields = ('in_house_no','description', 'product', 'purity', 'curr_amount', \
                 'm_unit', 'location', 'room', 'exp_date', 'ret_date',  'batch', \
-                 'in_house_no' 'del_date', 'open_date', 'company', 'cat_num')
+                 'del_date', 'open_date', 'company', 'cat_num')
 
         template_name = 'django_tables2/bootstrap.html'
 
